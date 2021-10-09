e5fa5336eb20da78186359a5a63fa56913fa8b93
jesus ortiz <ortizjesusrivas@gmail.com>
2021-10-08 23:03:01 -0400

    Wip

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-//add the router
-app.use(express.static('public'));
 require('dotenv').config();
-
+app.use(express.static('public'));
+app.use(cors({}));
+app.use('/test',(req,res)=>{
+    return res.send({
+        message : "aaaa"        
+    });
+});
 app.use('/', express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log(`app in progress localhost:${port}`);
+});
